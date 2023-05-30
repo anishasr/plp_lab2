93b36ad1872dc9d71f4168b8d2a9d028c0c05420
Anisha <anisha.srivastava123@gmail.com>
2023-05-30 18:09:56 -0500

    trying operations in C

--- dataTypeExperiment.c
+++ dataTypeExperiment.c
@@ -3,12 +3,30 @@
 
 int main()
 {
-    // int, float, char variables
+    // int, float
     int a = 12345;
     float b = 12345.6789;
+
+    // no built-in String type, array of chars
     char string123[] = "onetwothree";
 
     // boolean not a built-in type, imported
     bool isStudent = true;
     
+    // sum of int and float widens to float
+    float sum1 = a+b;
+    printf("%f", sum1);
+    
+    // elements in array/list must be of same type
+    int nums[] = {2,4,6,8};
+    char letters[] = {'a','b','c'};
+
+    // type conversions
+    int num = 85;
+    char numStr[10];
+    sprintf(numStr, "%d", num);
+    
+    // print numStr to verify conversion
+    printf("\nNow a string: %s", numStr);
+
 }
