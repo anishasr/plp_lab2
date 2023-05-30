79cb45d5c9e1e9916a847923574df3bfbc5198c0
Anisha <anisha.srivastava123@gmail.com>
2023-05-30 16:40:08 -0500

    now tracking file with code

---
+++ dataTypeExperiment.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ #include <stdbool.h>
+
+int main()
+{
+    // int, float, char variables
+    int a = 12345;
+    float b = 12345.6789;
+    char string123[] = "onetwothree";
+
+    // boolean not a built-in type, imported
+    bool isStudent = true;
+    
+}
